62bc0925ce044abd713ae53c10627a2004143ac0
InSeong-So <goflvhxj2547@gmail.com>
2020-07-13 21:51:51 +0900

    react-nodebird 수업 진행

--- ch1/front/pages/about.js
+++ ch1/front/pages/about.js
@@ -2,13 +2,9 @@ import React from 'react';
 
 const About = () => {
     return (
-        <Router>
-            <Switch>
-                <Route path="/user/create" component={Create}></Route>
-            </Switch>
-        </Router>
         <div>about!</div>
     );
 };
+
 
 export default About;
